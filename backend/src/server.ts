f6b4ce7f995952c169827d7805e63bbe6b92d6cd
RoeyTamano <roitamano@gmail.com>
2025-10-13 12:16:17 +0300

    ready for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import authRoutes from './routes/auth.route';
 import messageRoutes from './routes/message.route';
+import 'dotenv/config';
 const app = express();
 const port = parseInt(process.env.PORT || '3000', 10);
 if (isNaN(port) || port < 1 || port > 65535) {
@@ -12,10 +13,10 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 app.listen(port, () => {
